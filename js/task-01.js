c80b9afa049b539fed1ccbded9692f133aeca211
Ivan <nigaev.ivan@gmail.com>
2022-08-06 23:55:26 +0300

    task-01

---
+++ js/task-01.js
@@ -0,0 +1,16 @@
+const allCategoriesEl = document.querySelector('ul#categories');
+
+console.log(allCategoriesEl);
+
+const categoryItemsRef = document.querySelectorAll('.item');
+console.log('Number of categories:', categoryItemsRef.length);
+
+
+categoryItemsRef.forEach( (element) => {
+
+        const titleEl = element.firstElementChild.textContent;
+        console.log(`Category: ${titleEl}`);
+
+        const itemsEl = element.lastElementChild.children.length;
+        console.log(`Elements: ${itemsEl}`);
+});
