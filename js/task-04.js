b097acc788b0d95288015ceb8335822c0572f4e7
Ivan <nigaev.ivan@gmail.com>
2022-08-07 00:02:59 +0300

    task-04

---
+++ js/task-04.js
@@ -0,0 +1,13 @@
+let counterValue = 0
+const value = document.querySelector('#value')
+const minusValue = document.querySelector('button[data-action="decrement"]');
+const plusValue = document.querySelector('button[data-action="increment"]');
+
+
+const handndleMinusBtnClick = () => 
+{ counterValue -= 1; value.textContent = counterValue;}
+const handndlePlusBtnClick = () => 
+{ counterValue +=1; value.textContent = counterValue;}
+
+minusValue.addEventListener('click', handndleMinusBtnClick);
+plusValue.addEventListener('click', handndlePlusBtnClick);
